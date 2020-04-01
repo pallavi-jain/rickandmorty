b33864edce10073f860137240744e6fccb032263
pallavi-jain <pallavi.jain2610@gmail.com>
2020-04-01 17:53:08 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import CharactersList from './components/Characters/CharactersList';
+function App() {
+  return (
+    <div className="App">
+      <CharactersList/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Characters/CharactersList.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './CharactersList.css';
+
+class CharactersList extends Component{
+
+
+    render(){
+        return (
+            <div className="Test">Hello there..</div>
+        )
+    }
+}
+
+export default CharactersList;
